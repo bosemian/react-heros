f5f480e0ab9a9d78c90cccc9cb67613178f00b46
spby <siwanon.turbow@gmail.com>
2017-05-11 19:37:44 +0700

    renderHero doing

---
+++ heros.js
@@ -0,0 +1,124 @@
+var heros = [
+	{
+		"id": 1,
+		"name": "Butterfly",
+		"nickname": "",
+		"avatar": "./images/Butterfly/butterfly.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Butterfly/1.png"},
+			{ "skillname": "", img: "./images/Butterfly/2.png"},
+			{ "skillname": "", img: "./images/Butterfly/3.png"},
+			{ "skillname": "", img: "./images/Butterfly/4.png"}
+		]
+	},
+	{
+		"id": 2,
+		"name": "Changnar",
+		"nickname": "",
+		"avatar": "./images/Changnar/changnar.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Changnar/1.png"},
+			{ "skillname": "", img: "./images/Changnar/2.png"},
+			{ "skillname": "", img: "./images/Changnar/3.png"},
+			{ "skillname": "", img: "./images/Changnar/4.png"}
+		]
+	},
+	{
+		"id": 3,
+		"name": "Lubu",
+		"nickname": "",
+		"avatar": "./images/Lubu/lubu.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Lubu/1.png"},
+			{ "skillname": "", img: "./images/Lubu/2.png"},
+			{ "skillname": "", img: "./images/Lubu/3.png"},
+			{ "skillname": "", img: "./images/Lubu/4.png"}
+		]
+	},
+	{
+		"id": 4,
+		"name": "Mganga",
+		"nickname": "",
+		"avatar": "./images/Mganga/mganga.png",
+		"skill": [
+			{ "skillname": '', img: "./images/Mganga/1.png"},
+			{ "skillname": '', img: "./images/Mganga/2.png"},
+			{ "skillname": '', img: "./images/Mganga/3.png"},
+			{ "skillname": '', img: "./images/Mganga/4.png"}
+		]
+	},
+	{
+		"id": 5,
+		"name": "Mina",
+		"nickname": "",
+		"avatar": "./images/Mina/mina.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Mina/1.png"},
+			{ "skillname": "", img: "./images/Mina/2.png"},
+			{ "skillname": "", img: "./images/Mina/3.png"},
+			{ "skillname": "", img: "./images/Mina/4.png"}
+		]
+	},
+	{
+		"id": 6,
+		"name": "Omega",
+		"nickname": "",
+		"avatar": "./images/Omega/omega.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Omega/1.png"},
+			{ "skillname": "", img: "./images/Omega/2.png"},
+			{ "skillname": "", img: "./images/Omega/3.png"},
+			{ "skillname": "", img: "./images/Omega/4.png"}
+		]
+	},
+	{
+		"id": 7,
+		"name": "Thane",
+		"nickname": "",
+		"avatar": "./images/Thane/thane.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Thane/1.png"},
+			{ "skillname": "", img: "./images/Thane/2.png"},
+			{ "skillname": "", img: "./images/Thane/3.png"},
+			{ "skillname": "", img: "./images/Thane/4.png"}
+		]
+	},
+	{
+		"id": 8,
+		"name": "Van Helsing",
+		"nickname": "",
+		"avatar": "./images/VanHelsing/vanhelsing.png",
+		"skill": [
+			{ "skillname": "", img: "./images/VanHelsing/1.png"},
+			{ "skillname": "", img: "./images/VanHelsing/2.png"},
+			{ "skillname": "", img: "./images/VanHelsing/3.png"},
+			{ "skillname": "", img: "./images/VanHelsing/4.png"}
+		]
+	},
+	{
+		"id": 9,
+		"name": "Veera",
+		"nickname": "",
+		"avatar": "./images/Veera/veera.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Veera/1.png"},
+			{ "skillname": "", img: "./images/Veera/2.png"},
+			{ "skillname": "", img: "./images/Veera/3.png"},
+			{ "skillname": "", img: "./images/Veera/4.png"}
+		]
+	},
+	{
+		"id": 10,
+		"name": "Zephys",
+		"nickname": "",
+		"avatar": "./images/Zephys/zephys.png",
+		"skill": [
+			{ "skillname": "", img: "./images/Zephys/1.png"},
+			{ "skillname": "", img: "./images/Zephys/2.png"},
+			{ "skillname": "", img: "./images/Zephys/3.png"},
+			{ "skillname": "", img: "./images/Zephys/4.png"}
+		]
+	},
+]
+
+module.exports =  heros;
