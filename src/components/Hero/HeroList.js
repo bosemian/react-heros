eda107363a32e524fb8b42ea12fd48f9f36cccee
spby <siwanon.turbow@gmail.com>
2017-05-11 21:08:10 +0700

    custom style a skill

--- src/components/Hero/HeroList.js
+++ src/components/Hero/HeroList.js
@@ -4,23 +4,24 @@ import heros from '../../../heros'
 class HeroList extends Component {
 
     renderHero = () => {
-        return heros.map((hero) => {
+        return heros.map(hero => {
             return (
                 <div key={hero.id} className="column is-one-quarter">
                     <div className="card">
                         <div className="card-image">
                             <figure className="image is-4by3">
-                                <img src={hero.avatar} alt="avartar"/>
+                                <img src={hero.avatar} alt="avartar" title={hero.name} />
                             </figure>
                         </div>
                         <div className="card-content"> 
                                 <div className="media-left">
-                                    
+                                    <p className="title is-4 hero-name"><a className="is-primary">{hero.name}</a></p>
+                                    <p className="is-6">Skills</p>
                                     { this.renderSkill(hero.skill)}
 
                                 </div>
                             <div className="content">
-                                <p className="subtitle is-4"><a className="is-primary">{hero.name}</a></p>
+                                
                             </div>
                         </div>
                     </div>
